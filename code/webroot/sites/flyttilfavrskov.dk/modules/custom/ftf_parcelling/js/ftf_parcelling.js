2e4b6214ff9e88a003b1ecaf018fce5356d39dbb
Anita Jensen <ahj@novicell.dk>
2019-12-17 13:23:53 +0100

    adding block type gis minimap

--- code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_parcelling/js/ftf_parcelling.js
+++ code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_parcelling/js/ftf_parcelling.js
@@ -1,3 +1,11 @@
 (function (drupalSettings) {
-  new ParcelMap('parcel-map-body', drupalSettings.ftf_parcelling.parcelling_type, drupalSettings.ftf_parcelling.parcelling_id);
+  if(drupalSettings.ftf_parcelling.parcelling_type && drupalSettings.ftf_parcelling.parcelling_id) {
+    new ParcelMap('parcel-map-body', drupalSettings.ftf_parcelling.parcelling_type, drupalSettings.ftf_parcelling.parcelling_id);
+  }
+  if(drupalSettings.ftf_parcelling.gis_minimap) {
+    for(let key in drupalSettings.ftf_parcelling.gis_minimap) {
+      MiniMap.createMiniMap({mapDiv: 'gis_minimap_'+key, minimapId: drupalSettings.ftf_parcelling.gis_minimap[key]});
+
+    }
+  }
 })(drupalSettings);
