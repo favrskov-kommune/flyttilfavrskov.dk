006f21d3bc6d1e7cf63c7c49f6560207d273b1a8
Kasper Basse <kdb@novicell.dk>
2020-01-17 09:27:42 +0100

    Adding vertical align for two and three column

---
+++ code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_core/src/Plugin/Layout/ThreeColumnLayout.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\ftf_core\Plugin\Layout;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\dds_layouts\Plugin\Layout\DDSThreeColumnLayout;
+
+class ThreeColumnLayout extends DDSThreeColumnLayout {
+
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['vertical_align'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Vertical align content'),
+      '#default_value' => $this->configuration['vertical_align'] ?? FALSE,
+      '#description' => $this->t('Vertical align content for this layout'),
+    ];
+    return $form;
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the plugin form as built
+   *   by static::buildConfigurationForm().
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form. Calling code should pass on a subform
+   *   state created through
+   *   \Drupal\Core\Form\SubformState::createForSubform().
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['vertical_align'] = $form_state->getValue('vertical_align');
+  }
+
+  public function defaultConfiguration() {
+    $configuration = parent::defaultConfiguration();
+    $configuration['vertical_align'] = FALSE;
+    return $configuration;
+  }
+}
