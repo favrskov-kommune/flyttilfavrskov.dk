29ea32d00d042ea578b8331bae9a1b52ad18a212
Anita Jensen <ahj@novicell.dk>
2020-09-09 14:53:14 +0200

    FAVR2-80: implement submenu spot

--- code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_core/src/Plugin/Layout/OneColumnLayout.php
+++ code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_core/src/Plugin/Layout/OneColumnLayout.php
@@ -4,21 +4,16 @@ namespace Drupal\ftf_core\Plugin\Layout;
 
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\dds_layouts\Plugin\Layout\DDSThreeColumnLayout;
+use Drupal\dds_layouts\Plugin\Layout\DDSOneColumnLayout;
 
-class ThreeColumnLayout extends DDSThreeColumnLayout {
+class OneColumnLayout extends DDSOneColumnLayout {
 
   /**
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
-    $form['vertical_align'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Vertical align content'),
-      '#default_value' => $this->configuration['vertical_align'] ?? FALSE,
-      '#description' => $this->t('Vertical align content for this layout'),
-    ];
+
     $form['section_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Section ID'),
@@ -34,7 +29,6 @@ class ThreeColumnLayout extends DDSThreeColumnLayout {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
-    $this->configuration['vertical_align'] = $form_state->getValue('vertical_align');
     $this->configuration['section_id'] = Html::getId($form_state->getValue('section_id'));
   }
 
@@ -43,8 +37,8 @@ class ThreeColumnLayout extends DDSThreeColumnLayout {
    */
   public function defaultConfiguration() {
     $configuration = parent::defaultConfiguration();
-    $configuration['vertical_align'] = FALSE;
     $configuration['section_id'] = '';
     return $configuration;
   }
+
 }
--- code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_core/src/Plugin/Layout/TwoColumnLayout.php
+++ code/webroot/sites/flyttilfavrskov.dk/modules/custom/ftf_core/src/Plugin/Layout/TwoColumnLayout.php
@@ -2,11 +2,15 @@
 
 namespace Drupal\ftf_core\Plugin\Layout;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\dds_layouts\Plugin\Layout\DDSTwoColumnLayout;
 
 class TwoColumnLayout extends DDSTwoColumnLayout {
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
     $form['vertical_align'] = [
@@ -15,28 +19,32 @@ class TwoColumnLayout extends DDSTwoColumnLayout {
       '#default_value' => $this->configuration['vertical_align'] ?? FALSE,
       '#description' => $this->t('Vertical align content for this layout')
     ];
+    $form['section_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section ID'),
+      '#default_value' => $this->configuration['section_id'],
+      '#description' => 'Should only contain lowercase letters, numbers and hyphens.'
+    ];
+
     return $form;
   }
 
   /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the plugin form as built
-   *   by static::buildConfigurationForm().
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form. Calling code should pass on a subform
-   *   state created through
-   *   \Drupal\Core\Form\SubformState::createForSubform().
+   * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
     $this->configuration['vertical_align'] = $form_state->getValue('vertical_align');
+    $this->configuration['section_id'] = Html::getId($form_state->getValue('section_id'));
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function defaultConfiguration() {
     $configuration = parent::defaultConfiguration();
     $configuration['vertical_align'] = FALSE;
+    $configuration['section_id'] = '';
     return $configuration;
   }
 }
