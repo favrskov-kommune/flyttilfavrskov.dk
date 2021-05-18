332ec18c0cc6b94f9806abd7d64e37f1aeb1f7c0
Mikkel Mandal <mma@novicell.dk>
2021-05-18 16:09:46 +0200

    Fixing typo

--- .docker/php/release/mount_drupal_folders.sh
+++ .docker/php/release/mount_drupal_folders.sh
@@ -4,35 +4,30 @@
 if [[ ! -d /mnt/cache/php ]]; then
     mkdir /mnt/cache/php
 fi
-rm -rf /app/webroot/sites/beierholm.dk/files/php/*
-mount -o bind /mnt/cache/php /app/webroot/sites/beierholm.dk/files/php
+rm -rf /app/webroot/sites/flyttilfavrskov.dk/files/php/*
+mount -o bind /mnt/cache/php /app/webroot/sites/flyttilfavrskov.dk/files/php
 
 if [[ ! -d /mnt/cache/js ]]; then
     mkdir /mnt/cache/js
 fi
-rm -f /app/webroot/sites/beierholm.dk/files/js/optimized/.htaccess
-rm -rf /app/webroot/sites/beierholm.dk/files/js/*
-mount -o bind /mnt/cache/js /app/webroot/sites/beierholm.dk/files/js
+rm -f /app/webroot/sites/flyttilfavrskov.dk/files/js/optimized/.htaccess
+rm -rf /app/webroot/sites/flyttilfavrskov.dk/files/js/*
+mount -o bind /mnt/cache/js /app/webroot/sites/flyttilfavrskov.dk/files/js
 
 if [[ ! -d /mnt/cache/css ]]; then
     mkdir /mnt/cache/css
 fi
-rm -f /app/webroot/sites/beierholm.dk/files/css/optimized/.htaccess
-rm -rf /app/webroot/sites/beierholm.dk/files/css/*
-mount -o bind /mnt/cache/css /app/webroot/sites/beierholm.dk/files/css
+rm -f /app/webroot/sites/flyttilfavrskov.dk/files/css/optimized/.htaccess
+rm -rf /app/webroot/sites/flyttilfavrskov.dk/files/css/*
+mount -o bind /mnt/cache/css /app/webroot/sites/flyttilfavrskov.dk/files/css
 
 if [[ ! -d /mnt/cache/styles ]]; then
     mkdir /mnt/cache/styles
 fi
-rm -rf /app/webroot/sites/beierholm.dk/files/styles/*
-mount -o bind /mnt/cache/styles /app/webroot/sites/beierholm.dk/files/styles
+rm -rf /app/webroot/sites/flyttilfavrskov.dk/files/styles/*
+mount -o bind /mnt/cache/styles /app/webroot/sites/flyttilfavrskov.dk/files/styles
 
 echo "Cache mounts complete."
-
-# Mount FTP dir
-mount -o bind /mnt/ftp/beierholm /app/private-files/ftp
-
-echo "FTP mounts complete."
 
 # Mount logs dir
 if [[ ! -d /app/logs ]]; then
