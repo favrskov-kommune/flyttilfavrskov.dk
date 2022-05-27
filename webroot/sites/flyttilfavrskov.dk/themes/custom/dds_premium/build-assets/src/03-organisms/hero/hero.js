3a79ff449e8655c9596612aeabae45dfd6643973
Søren Due Bonde <sdb@novicell.dk>
2022-05-27 19:55:19 +0200

    FAVR2-158: Added playsinline attribute to html video markup

--- webroot/sites/flyttilfavrskov.dk/themes/custom/dds_premium/build-assets/src/03-organisms/hero/hero.js
+++ webroot/sites/flyttilfavrskov.dk/themes/custom/dds_premium/build-assets/src/03-organisms/hero/hero.js
@@ -14,10 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
           if (oembed.html.indexOf('iframe') > -1) {
             const regex = /<iframe.*?src="(.*?)"/;
             const oldSrc = regex.exec(oembed.html)[1];
-            const newSrc = `${oldSrc}&autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&background=1`; // We need to set this ourselves, otherwise we are not sure it is gonna play.
+            const newSrc = `${oldSrc}&autoplay=1&mute=1&playsinline=1&controls=0&showinfo=0&autohide=1&background=1`; // We need to set this ourselves, otherwise we are not sure it is gonna play.
             oembed.html = oembed.html.replace(oldSrc, newSrc);
           } else if (oembed.html.indexOf('video') > -1) {
-            oembed.html = oembed.html.replace('<video', '<video autoplay loop muted');
+            oembed.html = oembed.html.replace('<video', '<video autoplay playsinline loop muted');
           }
           iframeWrapper.innerHTML = oembed.html;
         }
